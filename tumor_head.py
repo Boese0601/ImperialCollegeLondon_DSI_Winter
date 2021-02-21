b47bd7b50db83de4f33b2d19161d64b4ff9ac268
Di Chang <61009212+Boese0601@users.noreply.github.com>
2021-02-21 13:09:20 +0800

    Add files via upload

--- tumor_head.py
+++ tumor_head.py
@@ -3,7 +3,6 @@ import random
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from keras_preprocessing.image.affine_transformations import *
 
 
 def load_data(path='../data/Dataset/', seg=False):
@@ -27,36 +26,11 @@ def load_data(path='../data/Dataset/', seg=False):
     return train_list, val_list
 
 
-def transform(x, transform_parameters=None):
-    x = apply_affine_transform(x,
-                               transform_parameters.get('theta', 0),
-                               transform_parameters.get('tx', 0),
-                               transform_parameters.get('ty', 0),
-                               transform_parameters.get('shear', 0),
-                               transform_parameters.get('zx', 1),
-                               transform_parameters.get('zy', 1),
-                               row_axis=0,
-                               col_axis=1,
-                               channel_axis=2)
-    if transform_parameters.get('channel_shift_intensity') is not None:
-        x = apply_channel_shift(x,
-                                transform_parameters['channel_shift_intensity'],
-                                2)
-    if transform_parameters.get('flip_horizontal', False):
-        x = flip_axis(x, 1)
-    if transform_parameters.get('flip_vertical', False):
-        x = flip_axis(x, 0)
-    return x
-
-
 class DataGenerator(tf.keras.utils.Sequence):
     """Generates data for Keras"""
 
     def __init__(self, list_IDs, batch_size=32, dim=(240, 240), n_channels=3,
-                 n_classes=2, shuffle=True, seg=False, channel_shift_range=0.0,
-                 height_shift_range=0.0, width_shift_range=0.0, rescale=None,
-                 horizontal_flip=False, vertical_flip=False, rotation_range=0,
-                 shear_range=0.0, zoom_range=0.0):
+                 n_classes=2, shuffle=True, seg=False):
         """Initialization"""
         self.dim = dim
         self.batch_size = batch_size
@@ -65,26 +39,6 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.n_classes = n_classes
         self.shuffle = shuffle
         self.seg = seg
-        self.channel_shift_range = channel_shift_range
-        self.height_shift_range = height_shift_range
-        self.width_shift_range = width_shift_range
-        self.rescale = rescale
-        self.hf = horizontal_flip
-        self.vf = vertical_flip
-        self.rr = rotation_range
-        self.shear_range = shear_range
-        self.zoom_range = zoom_range
-
-        if isinstance(zoom_range, (float, int)):
-            self.zoom_range = [1 - zoom_range, 1 + zoom_range]
-        elif (len(zoom_range) == 2 and
-              all(isinstance(val, (float, int)) for val in zoom_range)):
-            self.zoom_range = [zoom_range[0], zoom_range[1]]
-        else:
-            raise ValueError('`zoom_range` should be a float or '
-                             'a tuple or list of two floats. '
-                             'Received: %s' % (zoom_range,))
-
         self.on_epoch_end()
 
     def __len__(self):
@@ -110,78 +64,6 @@ class DataGenerator(tf.keras.utils.Sequence):
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def get_random_transform(self, seed=None):
-        """Generates random parameters for a transformation. Copied from ImageDataGenerator"""
-        img_row_axis = 0
-        img_col_axis = 1
-
-        if seed is not None:
-            np.random.seed(seed)
-
-        if self.rr:
-            theta = np.random.uniform(-self.rr, self.rr)
-        else:
-            theta = 0
-
-        if self.height_shift_range:
-            try:  # 1-D array-like or int
-                tx = np.random.choice(self.height_shift_range)
-                tx *= np.random.choice([-1, 1])
-            except ValueError:  # floating point
-                tx = np.random.uniform(-self.height_shift_range,
-                                       self.height_shift_range)
-            if np.max(self.height_shift_range) < 1:
-                tx *= self.dim[img_row_axis]
-        else:
-            tx = 0
-
-        if self.width_shift_range:
-            try:  # 1-D array-like or int
-                ty = np.random.choice(self.width_shift_range)
-                ty *= np.random.choice([-1, 1])
-            except ValueError:  # floating point
-                ty = np.random.uniform(-self.width_shift_range,
-                                       self.width_shift_range)
-            if np.max(self.width_shift_range) < 1:
-                ty *= self.dim[img_col_axis]
-        else:
-            ty = 0
-
-        if self.shear_range:
-            shear = np.random.uniform(
-                -self.shear_range,
-                self.shear_range)
-        else:
-            shear = 0
-
-        if self.zoom_range[0] == 1 and self.zoom_range[1] == 1:
-            zx, zy = 1, 1
-        else:
-            zx, zy = np.random.uniform(
-                self.zoom_range[0],
-                self.zoom_range[1],
-                2)
-
-        flip_horizontal = (np.random.random() < 0.5) * self.hf
-        flip_vertical = (np.random.random() < 0.5) * self.vf
-
-        channel_shift_intensity = None
-        if self.channel_shift_range != 0:
-            channel_shift_intensity = np.random.uniform(-self.channel_shift_range,
-                                                        self.channel_shift_range)
-
-        transform_parameters = {'theta': theta,
-                                'tx': tx,
-                                'ty': ty,
-                                'shear': shear,
-                                'zx': zx,
-                                'zy': zy,
-                                'flip_horizontal': flip_horizontal,
-                                'flip_vertical': flip_vertical,
-                                'channel_shift_intensity': channel_shift_intensity}
-
-        return transform_parameters
-
     def __data_generation(self, list_IDs_temp):
         """Generates data containing batch_size samples"""  # X : (n_samples, *dim, n_channels)
         # Initialization
@@ -189,20 +71,17 @@ class DataGenerator(tf.keras.utils.Sequence):
         if self.seg:
             y = np.empty((self.batch_size, *self.dim))
         else:
-            y = np.empty((self.batch_size,), dtype=int)
+            y = np.empty((self.batch_size), dtype=int)
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
-            img = np.load(ID)
-            # data augmentation
-            params = self.get_random_transform()
-            X[i,] = transform(img, params)
+            # TODO: add data augmentation
+            X[i, ] = np.load(ID)
 
             # Store class
             if self.seg:
-                label = np.expand_dims(np.load(ID[:-8] + '_seg.npy'), -1)
-                y[i] = np.squeeze(transform(label, params))
+                y[i] = np.load(ID[:-8] + '_seg.npy')
             else:
                 y[i] = min(1, np.sum(np.load(ID.split('_')[0] + '_seg.npy')))
 
@@ -268,9 +147,4 @@ def plot_history(history, batch_size):
 
 
 if __name__ == '__main__':
-    label = np.load('../data/Dataset/Yes/1_img.npy')
-    print(label.shape)
-    label = np.expand_dims(label, -1)
-    print(label.shape)
-    label = np.squeeze(label)
-    print(label.shape)
+    plot_samples('../data/Dataset')
--- tumor_segmentation.py
+++ tumor_segmentation.py
@@ -10,7 +10,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
-from tensorflow.keras.optimizers import Adam
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -21,30 +20,17 @@ print("[INFO] Loading data...")
 train_list, val_list = load_data(seg=True)
 
 batch_size = 8
-train_generator = DataGenerator(
-    train_list,
-    batch_size=batch_size,
-    # horizontal_flip=True,
-    # vertical_flip=True,
-    seg=True,
-    rotation_range=90,
-    #rescale=0.5,
-    # zoom_range=0.2,
-    #shear_range=0.5,
-    # width_shift_range=2,
-    # height_shift_range=2,
-)
+train_generator = DataGenerator(train_list, batch_size=batch_size, seg=True)
 validation_generator = DataGenerator(val_list, batch_size=batch_size, seg=True)
 IMG_SIZE = (240, 240)
 
 
-# TODO: changeodel (originally unet)
+# TODO: change model (originally unet)
 model = unet_3p()
-
 model.summary()
 
 # train model
-num_epochs = 110
+num_epochs = 20
 history = model.fit(
     train_generator,
     epochs=num_epochs,
